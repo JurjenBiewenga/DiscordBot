7770b395ea82fdf1b27c41fab744694ba5d21fd4
Jurjen Biewenga <j.f.biewenga@gmail.com>
2017-10-22 17:38:51 +0200

    Added command to get guildid

---
+++ DiscordBot/Commands/GuildId.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace DiscordBot.Commands
+{
+    public class GuildId :  ModuleBase<SocketCommandContext>
+    {
+        [Command("GetGuildID")]
+        [Alias("ggid")]
+        [Summary("Lists the guild id")]
+        public async Task GetGuildId()
+        {
+            await ReplyAsync(Context.Guild.Id.ToString());
+        }
+    }
+}
